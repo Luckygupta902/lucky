a66e0285b6e7d62d192d50bad8a660d7c023177e
lucky2301196 <lucky2301196@gmail.com>
2024-12-03 00:19:49 +0530

    First commit of autobg

---
+++ src/main/java/com/example/autobg2/Autobg2Application.java
@@ -0,0 +1,13 @@
+package com.example.autobg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autobg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Autobg2Application.class, args);
+	}
+
+}
